704b8584e3ad7cf5410c58bf434d6da208bf5d6a
VikramN <vikram@flux7.com>
2015-06-23 16:37:09 +0530

    added parameters for nginx vhost conf

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+##Run the script as follows
+## ./bootstrap.sh <domain name> < document root>
+## ./bootsrap.sh test.com /opt/test
+# This will create vhost file with server_name=test.com and document root as /opt/test
+########
+########
+DOMAIN=$1
+DOC_ROOT=$2
+###
 apt-get update
 apt-get install python-pip git unzip -y
 sudo apt-get install python-dev -y
@@ -6,7 +15,11 @@ sudo pip install paramiko PyYAML Jinja2 httplib2
 git clone git://github.com/ansible/ansible.git --recursive
 cd ./ansible
 source ./hacking/env-setup
+###
 wget https://s3.amazonaws.com/devpanel-scripts/ansible/nginx-php.zip
 unzip nginx-php.zip
 cd ansible-playbooks
-ansible-playbook -i "localhost," -c local boot.yml 
+#ansible-playbook -i "localhost," -c local boot.yml 
+ansible-playbook -i "localhost," -c local boot.yml --extra-vars '{"server_name": '\"$DOMAIN\"'}' --extra-vars '{"document_root": '\"$DOC_ROOT\"'}'
+###
+###
